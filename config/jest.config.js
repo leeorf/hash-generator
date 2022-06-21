1e9c5d9e34a7129cbf4000dfd425bdaf21d94247
Leonardo Rodrigues <leo.rf.dev@gmail.com>
2022-06-20 23:47:08 -0300

    chore: add src to jest roots
    This was necessary due to jest findRelatedTest implementation, to allow it to find the tests.

--- config/jest.config.js
+++ config/jest.config.js
@@ -2,7 +2,7 @@ const path = require('node:path');
 
 module.exports = {
   rootDir: path.resolve(__dirname, '..'),
-  roots: ['<rootDir>/__tests__'],
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: [
