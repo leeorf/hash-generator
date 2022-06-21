03b372f32ac940fb63ad3105868ef68d6ef365bd
Leonardo Rodrigues <leo.rf.dev@gmail.com>
2022-06-20 23:18:12 -0300

    chore: specify the allowed root and test name for Jest to search

--- config/jest.config.js
+++ config/jest.config.js
@@ -2,13 +2,14 @@ const path = require('node:path');
 
 module.exports = {
   rootDir: path.resolve(__dirname, '..'),
+  roots: ['<rootDir>/__tests__'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/__tests__/e2e',
   ],
-  roots: ['<rootDir>/src', '<rootDir>/__tests__/unit'],
+  testMatch: ['<rootDir>/**/*.test.ts?(x)'],
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest',
   },
